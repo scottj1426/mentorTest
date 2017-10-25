120b7035cf9e29a03d40b8b577bfb37bbd1225f7
James Scott <scottj1426@gmail.com>
2017-10-25 00:06:59 -0500

    done

--- toyProblems.js
+++ toyProblems.js
@@ -1,27 +1,61 @@
 //JS Basics
 //Run npm test in the command line to test your solutions
 module.exports = {
-  // reverseIt:
+   reverseIt:
+   function reverseIt(str){
+     return str.split('').reverse().join('')
+   },
 
   //uncomment and finish the reverseIt function. It will take in one parameter which is a String and
   //reverse it
 
-  // removeDups:
+   removeDups: function removeDups (arr) {
+    return arr.filter(function(num,i,arr){
+    return arr.indexOf(num) === i
+   })
+ },
 
   //uncomment and finish the removeDups function. It will take in one parameter which is an Array
   //remove all duplicates
 
-  // titleIt:
+   titleIt: 
+   function titleCase(str) {  
+    str = str.toLowerCase().split(' ');
+  
+    for(var i = 0; i < str.length; i++){
+      str[i] = str[i].split('');
+      str[i][0] = str[i][0].toUpperCase(); 
+      str[i] = str[i].join('');
+    }
+    return str.join(' ');
+  },
 
   //uncomment and finish the titleIt function. It will take in one parameter which is a String and
   //capitalize the first letter of each word
 
-  // vowelCounter:
+   vowelCounter:
+   function vowelCounter(str){
+    var count = 0;
+    for(var i = 0; i < str.length; i++){
+      if(str[i].toLowerCase() == 'a' || str[i].toLowerCase() == 'i' || str[i].toLowerCase() == 'o' ||str[i].toLowerCase() == 'e' ||str[i].toLowerCase() == 'u'){
+        count+=1;
+      }
+    }
+   return count;
+  },
+
 
   //uncomment and finish the vowelCounter function. It will take in one parameter which is a String and
   //return the number of vowels in the string
 
-  // isPrime:
+   isPrime:
+   function isPrime(num){
+     for(var i = 2; i < num; i++){
+       if(num %  i ===0)
+       return false; 
+     }
+     return true; 
+   },
 
   //uncomment and finish the isPrime function. It will take in one parameter which is a Number and
   //return true if it is prime and false if it is not
@@ -30,7 +64,7 @@ module.exports = {
   //var foo = 10 + '20';
   //uncomment the foo property and give your answer as its value
 
-  //foo:
+  foo: '1020',
 
   //what is the outcome of the two console.logs below?
   //   var foo = "Hello";
@@ -41,6 +75,6 @@ module.exports = {
   // console.log(foo + bar);
   // uncomment the log1 and log2 properties and give your answers as their values
 
-  // log1:
-  // log2:
+   log1: 'Hello World',
+   log2: Undefined
 }
